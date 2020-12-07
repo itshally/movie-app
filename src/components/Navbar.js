1091acccec964bb7c495f641bd10dec60676ae1a
Hally <hbianan@gmail.com>
2020-12-06 20:15:35 -0500

    added logo and favicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import {Navbar} from 'react-bootstrap'
 import moment from 'moment'
+import logo from '../assets/favicon.png'
 const Navs = () => {
     const today = new Date();
 
     return (
         <React.Fragment>
             <Navbar bg="dark" variant="dark" fixed="top">
-                <Navbar.Brand href="#">Trending Movies</Navbar.Brand>
+                <Navbar.Brand href="#">
+                    <img
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="logo"
+                        style={{marginRight: '5px', borderRadius: '4px'}}
+                    />
+                    Trending Movies
+                </Navbar.Brand>
                 <Navbar.Text>
                     {moment(today).format('MMMM DD YYYY')}
                 </Navbar.Text>
