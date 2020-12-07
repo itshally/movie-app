e62a35b37bc3180e848f365fe8b2037b7488e96b
Hally <hbianan@gmail.com>
2020-12-06 20:15:12 -0500

    files updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import './styles/style.css'
+import {Nav, Header, MovieList, Footer} from './components/index'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = (props) => {
+    return (
+        <div className="App">
+            <Nav/>
+            <Header/>
+            <MovieList/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
