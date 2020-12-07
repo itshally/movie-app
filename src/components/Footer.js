c0496e3819443adbe3667ede6bbf28d37c9358f7
Hally <hbianan@gmail.com>
2020-12-06 20:15:51 -0500

    fixed margin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import {Container} from 'react-bootstrap'
 const Footer = () => {
     return (
         <Container className="footer" fluid={true}>
-            <p>&copy; 2020 Hally. All Rights Reserved. Powered by
+            <p>&copy; 2020 Hally. All Rights Reserved. Powered by&nbsp;
                 <a href="https://www.vercel.com/" target="_blank" rel="noopener noreferrer">Vercel</a>.</p>
         </Container>
     )
